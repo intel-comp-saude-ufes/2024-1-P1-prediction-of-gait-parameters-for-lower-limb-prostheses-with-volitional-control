fdc91c6de52e7cc937caf4026f41e76843112c7b
fiorotticaio <caiofiorotti@gmail.com>
2024-05-24 11:11:41 -0300

    :bulb: adding comments in functions

--- dataset2/main.py
+++ dataset2/main.py
@@ -35,6 +35,7 @@ def load_train_data(train_folder):
         data_angles (pd.DataFrame): Angles data
     '''
 
+    # Counters to check if the data was already loaded at least once or nots
     count_emg = 0
     count_torques = 0
     count_grf = 0
@@ -42,11 +43,11 @@ def load_train_data(train_folder):
 
     for root, dirs, files in os.walk(train_folder):
         for file in files:
+            # Pass from each file in the folder
             if file.endswith('.csv'):
-                file_path = os.path.join(root, file)
-                print(f'Loading file: {file_path}')
+                file_path = os.path.join(root, file) # Get the hole file path
                 if 'emg_filtered' in file_path:
-                    if count_emg == 0:
+                    if count_emg == 0: # If the data was not loaded yet
                         data_emg = pd.read_csv(file_path)
                     else:
                         data_emg = pd.concat([data_emg, pd.read_csv(file_path)], axis=0)
@@ -73,16 +74,27 @@ def load_train_data(train_folder):
     return data_emg, data_torques, data_grf, data_angles
 
 
-# Plotar comparações entre os modelos usando subplots
 def plot_comparisons(y_true, predictions, metrics):
+    '''
+    Function to plot the comparison between the real values and the predicted values
+
+    INPUT:
+        y_true (pd.Series): Real values
+        predictions (dict): Dictionary containing the predictions of each model
+        metrics (dict): Dictionary containing the R^2 score of each model
+
+    OUTPUT:
+        None
+    '''
+
     plt.figure(figsize=(18, 10))
     
     for i, (model_name, y_pred) in enumerate(predictions.items(), 1):
         plt.subplot(3, 1, i)
         plt.plot(range(len(y_true)), y_true, label='Real', color='blue')
         plt.plot(range(len(y_pred)), y_pred, label=model_name, linestyle='dashed', color='red')
-        plt.xlabel('Amostras')
-        plt.ylabel('Angulação do Joelho (Graus)')
+        plt.xlabel('Samples')
+        plt.ylabel('Knee Angulation (Degrees)')
         plt.legend()
         plt.title(f'{model_name} - R^2 Score: {metrics[model_name]:.2f}')
     
