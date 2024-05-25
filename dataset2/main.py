a7159dc94ec3f24402ced4d332ea336ce829082a
fiorotticaio <caiofiorotti@gmail.com>
2024-05-25 09:24:59 -0300

    :bulb: update comments

--- dataset2/main.py
+++ dataset2/main.py
@@ -161,7 +161,7 @@ if __name__ == '__main__':
         'MLP Regressor' : MLPRegressor(hidden_layer_sizes=(100,), activation='relu', solver='adam', alpha=0.0001, batch_size='auto', learning_rate='constant', learning_rate_init=0.001, power_t=0.5, max_iter=200, shuffle=True, random_state=None, tol=0.0001, verbose=False, warm_start=False, momentum=0.9, nesterovs_momentum=True, early_stopping=False, validation_fraction=0.1, beta_1=0.9, beta_2=0.999, epsilon=1e-08, n_iter_no_change=10, max_fun=15000),
     }
 
-    # Dictionary to store the predictions and metrics to plot
+    # Dictionary to store the predictions and metrics to plot after
     predictions = {}
     metrics = {}
 
