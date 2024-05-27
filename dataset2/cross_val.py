accb25970d03850d0ace8491eb173d395b426acf
fiorotticaio <caiofiorotti@gmail.com>
2024-05-27 18:18:57 -0300

    :bookmark: changing var names

--- dataset2/cross_val.py
+++ dataset2/cross_val.py
@@ -2,10 +2,10 @@ import os
 import numpy as np
 import pandas as pd
 from xgboost import XGBRegressor
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.metrics import r2_score
 
 # Função para carregar os dados dos arquivos
-def load_data(folder_path, train_files):
+def load_data(folder_path, files_to_load):
     # Counters to check if the data was already loaded at least once or nots
     count_angles = 0
     count_emg_envelope = 0
@@ -19,7 +19,7 @@ def load_data(folder_path, train_files):
             if dir == 'Angles':
                 # Read the files inside this folder
                 for file in os.listdir(os.path.join(root, dir)):
-                    if file in train_files:
+                    if file in files_to_load:
                         if count_angles == 0:
                             data_angles = pd.read_csv(os.path.join(root, dir, file), delimiter='\t')
                             count_angles += 1
@@ -29,7 +29,7 @@ def load_data(folder_path, train_files):
             elif dir == 'EMG envelope':
                 # Read the files inside this folder
                 for file in os.listdir(os.path.join(root, dir)):
-                    if file in train_files:
+                    if file in files_to_load:
                         if count_emg_envelope == 0:
                             data_emg_envelope = pd.read_csv(os.path.join(root, dir, file), delimiter='\t')
                             count_emg_envelope += 1
@@ -39,7 +39,7 @@ def load_data(folder_path, train_files):
             elif dir == 'EMG filtered':
                 # Read the files inside this folder
                 for file in os.listdir(os.path.join(root, dir)):
-                    if file in train_files:
+                    if file in files_to_load:
                         if count_emg_filtered == 0:
                             data_emg_filtered = pd.read_csv(os.path.join(root, dir, file), delimiter='\t')
                             count_emg_filtered += 1
@@ -49,7 +49,7 @@ def load_data(folder_path, train_files):
             elif dir == 'GRF':
                 # Read the files inside this folder
                 for file in os.listdir(os.path.join(root, dir)):
-                    if file in train_files:
+                    if file in files_to_load:
                         if count_grf == 0:
                             data_grf = pd.read_csv(os.path.join(root, dir, file), delimiter='\t')
                             count_grf += 1
@@ -59,7 +59,7 @@ def load_data(folder_path, train_files):
             elif dir == 'Torques':
                 # Read the files inside this folder
                 for file in os.listdir(os.path.join(root, dir)):
-                    if file in train_files:
+                    if file in files_to_load:
                         if count_torques == 0:
                             data_torques = pd.read_csv(os.path.join(root, dir, file), delimiter='\t')
                             count_torques += 1
@@ -69,7 +69,7 @@ def load_data(folder_path, train_files):
             elif dir == 'Torques_Norm':
                 # Read the files inside this folder
                 for file in os.listdir(os.path.join(root, dir)):
-                    if file in train_files:
+                    if file in files_to_load:
                         if count_torques_norm == 0:
                             data_torques_norm = pd.read_csv(os.path.join(root, dir, file), delimiter='\t')
                             count_torques_norm += 1
@@ -80,9 +80,8 @@ def load_data(folder_path, train_files):
     
 
 # Função para treinar e validar o modelo
-def train_and_validate(train_files, val_file):
-    train_folder = 'data/P3'
-    data_angles, data_emg_envelope, data_emg_filtered, data_grf, data_torques, data_torques_norm = load_data(train_folder, train_files)
+def train_and_validate(data_path, train_files, val_file):
+    data_angles, data_emg_envelope, data_emg_filtered, data_grf, data_torques, data_torques_norm = load_data(data_path, train_files)
 
     St = 'St1'
     data_emg_columns = [St+'_VL', St+'_BF']
@@ -105,8 +104,7 @@ def train_and_validate(train_files, val_file):
     model.fit(X_train, y_train)
 
 
-    val_folder = 'data/P3'
-    data_angles_val, data_emg_envelope_val, data_emg_filtered_val, data_grf_val, data_torques_val, data_torques_norm_val = load_data(val_folder, val_file)
+    data_angles_val, data_emg_envelope_val, data_emg_filtered_val, data_grf_val, data_torques_val, data_torques_norm_val = load_data(data_path, val_file)
 
     data_emg_envelope_val = data_emg_envelope_val[data_emg_columns]
     data_emg_filtered_val = data_emg_filtered_val[data_emg_columns]
@@ -126,45 +124,16 @@ def train_and_validate(train_files, val_file):
     
     return model
 
-# Função para testar o modelo
-def test_model(model, test_file):
-    # Prepare the test data
-    test_folder = 'data/P3'
-    data_angles_test, data_emg_envelope_test, data_emg_filtered_test, data_grf_test, data_torques_test, data_torques_norm_test = load_data(test_folder, test_file)
-
-    St = 'St1'
-    data_emg_columns = [St+'_VL', St+'_BF']
-    data_torques_columns = [St+'_Knee_X']
-    data_grf_columns = [St+'_GRF_X']
-    data_angles_columns = [St+'_Knee_X']
-
-    data_emg_envelope_test = data_emg_envelope_test[data_emg_columns]
-    data_emg_filtered_test = data_emg_filtered_test[data_emg_columns]
-    data_grf_test = data_grf_test[data_grf_columns]
-    data_angles_test = data_angles_test[data_angles_columns]
-    data_torques_test = data_torques_test[data_torques_columns]
-    data_torques_norm_test = data_torques_norm_test[data_torques_columns]
-
-    X_test = pd.concat([data_emg_envelope_test], axis=1) # Concatenate the input model data
-    y_test = data_angles_test # Get the target data
-
-    y_test_pred = model.predict(X_test)
-
-    r2 = r2_score(y_test, y_test_pred)
-    print(f'Test r2 for {test_file}: {r2}')
 
 
 if __name__ == '__main__':
     # Lista dos arquivos de dados
-    data_files = ['T1.txt', 'T2.txt', 'T3.txt', 'T4.txt', 'T5.txt', 'T6.txt', 'T7.txt', 'T8.txt', 'T9.txt']
+    data_path = 'data/P5'
+    data_files = ['T1.txt', 'T2.txt', 'T3.txt', 'T4.txt', 'T5.txt', 'T6.txt', 'T7.txt', 'T8.txt', 'T9.txt', 'T10.txt']
 
     # Validar deixando um arquivo de fora
     for i in range(len(data_files)):
         train_files = [f for j, f in enumerate(data_files) if j != i]
         val_file = data_files[i]
         print(f'\nTraining with {train_files}, validating with {val_file}')
-        model = train_and_validate(train_files, val_file)
-
-    # Testar com o arquivo T10.txt
-    print('\nTesting with T10.txt')
-    test_model(model, 'T10.txt')
+        model = train_and_validate(data_path, train_files, val_file)
